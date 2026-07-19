291be2d7b9b394c9e6c226091315830675e410f3
Udit Sharma <uditsharma9981@gmail.com>
2026-07-19 22:49:27 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,8 +3,6 @@ from crew import LocalAiFinancialAnalysisSystemCrew
 
 # This main file is intended to be a way for your to run your
 # crew locally, so refrain from adding unnecessary logic into this file.
-# Replace with inputs you want to test with, it will automatically
-# interpolate any tasks and agents information
 
 def run():
     """
